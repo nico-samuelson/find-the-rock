fdfee14a4484777a95b16d10e975d5d7516014f7
nico-samuelson <nicos.t.65@gmail.com>
2024-05-28 15:27:47 +0700

    Initial Commit

---
+++ FindTheRocks/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FindTheRocks
+//
+//  Created by Nico Samuelson on 28/05/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
