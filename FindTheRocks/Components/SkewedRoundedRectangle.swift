594ee8c4a27575d432185007feb0e73756bca23c
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-05-28 23:30:46 +0700

    feat: add customization shape

--- FindTheRocks/Components/SkewedRoundedRectangle.swift
+++ FindTheRocks/Components/SkewedRoundedRectangle.swift
@@ -24,45 +24,73 @@ struct SkewedRoundedRectangle: Shape {
     var bottomLeftXOffset:CGFloat = 0
     var bottomLeftYOffset:CGFloat = 0
     
-    var cornerRadius: CGFloat = 20
+    var topLeftCornerRadius: CGFloat = 0
+    var topRightCornerRadius: CGFloat = 0
+    var bottomLeftCornerRadius: CGFloat = 0
+    var bottomRightCornerRadius: CGFloat = 0
+    
+    var cornerRadius: CGFloat? = nil
     
     func path(in rect: CGRect) -> Path {
+        let tlCornerRadius = cornerRadius ?? topLeftCornerRadius
+        let trCornerRadius = cornerRadius ?? topRightCornerRadius
+        let blCornerRadius = cornerRadius ?? bottomLeftCornerRadius
+        let brCornerRadius = cornerRadius ?? bottomRightCornerRadius
+        
         var path = Path()
         
-        let width = rect.width
-        let height = rect.height
+        let width:CGFloat = rect.width
+        let height:CGFloat = rect.height
         
         // Start at the top-left corner
-        path.move(to: CGPoint(x: topLeftXOffset , y: cornerRadius))
+        path.move(to: CGPoint(x: topLeftXOffset , y: tlCornerRadius + topLeftYOffset))
         
         // Top-left rounded corner
-        path.addArc(center: CGPoint(x: cornerRadius + topLeftXOffset, y: cornerRadius + topLeftYOffset), radius: cornerRadius, startAngle: .degrees(180), endAngle: .degrees(270), clockwise: false)
+//        path.addArc(center: CGPoint(x: cornerRadius + topLeftXOffset, y: cornerRadius + topLeftYOffset), radius: cornerRadius, startAngle: .degrees(180), endAngle: .degrees(270), clockwise: false)
+        path.addQuadCurve(
+            to: CGPoint(x: topLeftXOffset + tlCornerRadius, y: topLeftYOffset),
+            control: CGPoint(x: topLeftXOffset + (topLeftXOffset - bottomLeftXOffset)*0.08, y: topLeftYOffset + (topLeftYOffset - topRightYOffset)*0.15)
+        )
+        
         
         // Top side
-        path.addLine(to: CGPoint(x: width - cornerRadius - topRightXOffset, y: 0 + topRightYOffset))
+        path.addLine(to: CGPoint(x: width - trCornerRadius - topRightXOffset, y: topRightYOffset))
         
         // Top-right rounded corner
-        path.addArc(center: CGPoint(x: width - cornerRadius - topRightXOffset, y: cornerRadius + topRightYOffset), radius: cornerRadius, startAngle: .degrees(270), endAngle: .degrees(360), clockwise: false)
+//        path.addArc(center: CGPoint(x: width - cornerRadius - topRightXOffset, y: cornerRadius + topRightYOffset), radius: cornerRadius, startAngle: .degrees(270), endAngle: .degrees(360), clockwise: false)
+        path.addQuadCurve(
+            to: CGPoint(x: width - topRightXOffset, y: topRightYOffset + trCornerRadius),
+            control: CGPoint(x: width - topRightXOffset - (topRightXOffset - bottomRightXOffset)*0.08, y: topRightYOffset + (topRightYOffset - topLeftYOffset)*0.15)
+        )
+        
         
         // Right side skewed
-        path.addLine(to: CGPoint(x: width - bottomRightXOffset, y: height - cornerRadius - bottomRightYOffset))
+        path.addLine(to: CGPoint(x: width - bottomRightXOffset, y: height - brCornerRadius - bottomRightYOffset))
         
         // Bottom-right rounded corner
-        path.addArc(center: CGPoint(x: width - cornerRadius - bottomRightXOffset, y: height - cornerRadius - bottomRightYOffset), radius: cornerRadius, startAngle: .degrees(0), endAngle: .degrees(90), clockwise: false)
+//        path.addArc(center: CGPoint(x: width - cornerRadius - bottomRightXOffset, y: height - cornerRadius - bottomRightYOffset), radius: cornerRadius, startAngle: .degrees(0), endAngle: .degrees(90), clockwise: false)
+        path.addQuadCurve(
+            to: CGPoint(x: width - brCornerRadius - bottomRightXOffset, y: height - bottomRightYOffset),
+            control: CGPoint(x: width - bottomRightXOffset - (bottomRightXOffset - bottomLeftXOffset)*0.08, y: height - bottomRightYOffset - (bottomRightYOffset - topRightYOffset)*0.15)
+        )
         
         // Bottom side
-        path.addLine(to: CGPoint(x: cornerRadius + bottomLeftXOffset, y: height - bottomLeftYOffset))
+        path.addLine(to: CGPoint(x: blCornerRadius + bottomLeftXOffset, y: height - bottomLeftYOffset))
         
         // Bottom-left rounded corner
-        path.addArc(center: CGPoint(x: cornerRadius + bottomLeftXOffset, y: height - cornerRadius - bottomLeftYOffset), radius: cornerRadius, startAngle: .degrees(90), endAngle: .degrees(180), clockwise: false)
+//        path.addArc(center: CGPoint(x: cornerRadius + bottomLeftXOffset, y: height - cornerRadius - bottomLeftYOffset), radius: cornerRadius, startAngle: .degrees(90), endAngle: .degrees(180), clockwise: false)
+        path.addQuadCurve(
+            to: CGPoint(x: bottomLeftXOffset, y: height - blCornerRadius - bottomLeftYOffset),
+            control: CGPoint(x: bottomLeftXOffset + (bottomLeftXOffset - topLeftXOffset)*0.08, y: height - bottomLeftYOffset - (bottomLeftYOffset - bottomRightYOffset)*0.15)
+        )
         
         // Left side
-        path.addLine(to: CGPoint(x: 0 + topLeftXOffset, y: cornerRadius + topLeftYOffset))
+        path.addLine(to: CGPoint(x: 0 + topLeftXOffset, y: tlCornerRadius + topLeftYOffset))
         
         return path
     }
 }
 #Preview {
-    SkewedRoundedRectangle()
+    SkewedRoundedRectangle(topLeftCornerRadius:20)
         .padding(100)
 }
