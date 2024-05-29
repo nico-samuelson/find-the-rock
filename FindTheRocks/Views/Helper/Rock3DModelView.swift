808d4a0b3dd9e4e712d7a59e8832f5a60f78adbf
sidipraptama <sidiaurelius@gmail.com>
2024-05-29 14:40:31 +0700

    tes

---
+++ FindTheRocks/Views/Helper/Rock3DModelView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  FindTheRocks
+//
+//  Created by Sidi Praptama Aurelius Nurhalim on 29/05/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
